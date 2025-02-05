b1b699080af37c8b12d259a8bba7fca59beb9df1
Mayank aneja <anejamayank16@gmail.com>
2025-02-05 22:51:28 +0530

    Initalised prisma

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,26 @@
 import { Hono } from 'hono'
+import z from "zod"
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.post("api/v1/signup",(c)=>{
+  
+  const email=c.req.param('email')
+  const name=c.req.param('name')
+
+  const body=z.object({
+    name:z.string(),
+    email:z.string()
+  })
+
+  const {success}=body.safeParse(body)
+
+  return c.text("signup route")
+})
+
 
 export default app

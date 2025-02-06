50b9ef4098b0f46edb5b7fdc0523c619eacedf59
Mayank aneja <anejamayank16@gmail.com>
2025-02-06 10:45:25 +0530

    commited after changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,26 +1,70 @@
 import { Hono } from 'hono'
 import z from "zod"
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign,decode,verify} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET:string
+  }
+}>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post("api/v1/signup",(c)=>{
-  
-  const email=c.req.param('email')
-  const name=c.req.param('name')
+app.post("api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
 
-  const body=z.object({
-    name:z.string(),
-    email:z.string()
+  const userSchema = await c.req.json()
+  const body = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(8)
   })
 
-  const {success}=body.safeParse(body)
+  const { success } = userSchema.safeParse(body)
 
-  return c.text("signup route")
+  if (!success) {
+    return c.json({
+      message:"Invalid arguments"
+    })
+  }
+  else {
+    const user = await prisma.user.create({
+      data: {
+        name: userSchema.name,
+        email: userSchema.email,
+        password: userSchema.password
+      }
+    })
+
+    const token=sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({
+      token:token
+    })
+  }
+  
 })
 
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+  const user = await prisma.user.findFirst({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  })
+  if (!user) {
+    return c.text("Invalid arguments")
+  }
+})
 
 export default app
